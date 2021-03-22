b8c6cc7840b97dbf05a6902941d761a16ef04f7a
EduRomeroA <81062470+EduRomeroA@users.noreply.github.com>
2021-03-21 23:33:27 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_DATOS: 'http://192.168.100.116/GET/guardar'
 };
 
 /*
