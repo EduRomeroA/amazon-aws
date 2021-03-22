f175a0701e94d8aaf8de691c06a125753302fd35
EduRomeroA <81062470+EduRomeroA@users.noreply.github.com>
2021-03-22 00:37:49 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http' 
 
+const API_DATOS = environment.API_REPORT;
+
+@InJectable({
+  provideIn: 'root'
+})
+
+export class Datos{
+  headers= new HttpHeaders({
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'});
+  constructor(private http: HttpClient){}
+find(){
+  return this.http.get(API_DATOS);
+}
+  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'La pagina de Prueba de Temperatura';
 }
