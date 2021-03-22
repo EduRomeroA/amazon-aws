eb1a3f0a41acf9c1593dba7fe46c2d23e6318f30
EduRomeroA <81062470+EduRomeroA@users.noreply.github.com>
2021-03-22 00:09:35 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot([
+      {path: ''}
+      ])
     BrowserModule,
     AppRoutingModule
   ],
